2543ab96e51f4b8722bdc7f861339f69d2638a58
cmdPromptCritical <7504765+cmdPromptCritical@users.noreply.github.com>
2020-05-10 00:27:16 -0400

    added ocrTxt logging, added multiprocessing

--- watch.py
+++ watch.py
@@ -1,7 +1,8 @@
 import time, os, csv
 import cv2
 import pytesseract
-import logging
+import logging, traceback
+import multiprocessing
 from wand.image import Image
 from wand.display import display
 from wand.color import Color
@@ -22,8 +23,32 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Richard\AppData\Local\Tessera
 # LOG FILE DIRECTORY:
 logging.basicConfig(filename='converted.log', filemode='w', level=logging.INFO)
 # OCR TXT FILE PATH
-ocrTxtFilePath = 'ocrTxt.csv'
+ocrTxtFilePath = 'ocrTxt.csv' # logs suscessful CORs
+ocrErrFilePath = 'ocrErr.csv' # logs failed OCRs
+
 #### END CONFIG 
+
+def ocrTxtListener(q):
+    try:
+        # listens for messages on q whenever OCR has been made, writes to file
+        with open(ocrTxtFilePath, 'a', newline='') as f: # newline='' fixes double space between entries
+            w = csv.writer(f)
+            while 1:
+                m = q.get()
+                w.writerow(m)
+
+    except Exception:
+        traceback.print_exc()
+        print('ERROR: listener failed. OCR text logging will no longer be performed')
+    
+def dummyFn(x):
+    print('starting job')
+    try:
+        v += 1
+    except Exception:
+        traceback.print_exc()
+    time.sleep(5)
+    print(f'job done! Ans: {x}')
 
 def remove(path):
     # removes file or folder recursively when passed an absolute path
@@ -37,73 +62,90 @@ def remove(path):
 
 def on_created(event):
     if event.src_path[-4:] == '.pdf' or event.src_path[-4:] == '.PDF':
-        print(f'candidate: {event.src_path[-4:]}')
-        ocrPdf(event.src_path)
-
-
+        #pool.apply_async(dummyFn, args=(event.src_path,))
+        pool.apply_async(ocrPdf, args=(event.src_path, q,))
+        
 def on_deleted(event):
-    print(f"what the h**k! Someone deleted {event.src_path}!")
+    #print(f"what the h**k! Someone deleted {event.src_path}!")
+    pass
 
 def on_modified(event):
     print(f"hey buddy, {event.src_path} has been modified")
+    pass
 
 def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+    #print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+    pass
 
-def ocrPdf(src_path):
-    filePathEnding = src_path.rfind('\\', )
-    filename = src_path[filePathEnding+1:] # filename only miight be needed
-    filePath = src_path[:filePathEnding] # filepath only miight be needed
-    filenameWoExt = filename[0:-4]
-    extracted_text = ' ' # holds OCR output
-    csvFile = open(ocrTxtFilePath, 'a')
-    ocrTxt = csv.writer(csvFile) # writes OCR output
+def ocrPdf(src_path, q):
+    # to print out errors when called by apply_async, a try/except needs to be wrapped around entire function
+    try:
+        filePathEnding = src_path.rfind('\\', )
+        filename = src_path[filePathEnding+1:] # filename only miight be needed
+        filePath = src_path[:filePathEnding] # filepath only miight be needed
+        filenameWoExt = filename[0:-4]
+        extracted_text = ' ' # holds OCR output
 
-    # excludes matches in the temp and exported folders
-    # note: current setup will also exclude any other folder starting with 'tmp' and 'exported'
-    print(f'checking if it\'s a tmp filepath: {filePath[:5]}')
-    if filePath[:6] == '.\\tmp\\':
-        print(f'skipping {src_path}: it\'s a temp file')
-        return f'skipping {src_path}: it\'s a temp file'
-    if filePath[:10] == '.\\exported':
-        print(f'skipping {src_path}: it\'s an exported file')
-        return f'skipping {src_path}: it\'s an exported file'
-    
-    print(f"hey, {src_path} has been created!")
+        # logging
+        #errFile = open(ocrErrFilePath, 'a', newline='') # newline='' fixes double space between entries
+        #errTxt = csv.writer(errFile) # writes list of failed OCR attempts
 
-    time.sleep(2) # allows for file to be copied
-    tmpDir = f'.\\tmp\\{filenameWoExt}'
-    if not os.path.exists(tmpDir):
-        os.mkdir(tmpDir)
+        # excludes matches in the temp and exported folders
+        # note: current setup will also exclude any other folder starting with 'tmp' and 'exported'
+        #print(f'checking if it\'s a tmp filepath: {filePath[:5]}')
+        if filePath[:6] == '.\\tmp\\':
+            print(f'skipping {src_path}: it\'s a temp file')
+            return f'skipping {src_path}: it\'s a temp file'
+        if filePath[:10] == '.\\exported':
+            print(f'skipping {src_path}: it\'s an exported file')
+            return f'skipping {src_path}: it\'s an exported file'
+        
+        print(f"processing: {src_path}")
 
-    tiffFiles = shredPdf(src_path, tmpDir)
-    if len(tiffFiles) > 0: # confirms there were no errors shredding
-        # pre-process image
-        tmpPdfFiles = []
-        for img_path in tiffFiles:
-            processedImg = preProcess(img_path)
+        #time.sleep(2) # allows for file to be copied
+        tmpDir = f'.\\tmp\\{filenameWoExt}'
+        if not os.path.exists(tmpDir):
+            os.mkdir(tmpDir)
 
-            # OCR it! psm = 1 will recognize 2-column layouts.
-            pdf = pytesseract.image_to_pdf_or_hocr(processedImg, extension='pdf', config='--psm 1')
-            text = pytesseract.image_to_string(processedImg,lang='eng', config='--psm 1')
-            # save extracted text to variable
-            print(text)
-            extracted_text = extracted_text + text
-            # save pdf to temp path
-            tmpSaveDir = f'{img_path[:-5]}.pdf'
-            with open(tmpSaveDir, 'w+b') as f:
-                f.write(pdf) # pdf type is bytes by default
-                print(f'saved temp pdf: {tmpSaveDir}')
-                tmpPdfFiles.append(tmpSaveDir)
-        mergePdfs(tmpPdfFiles)
-        # save ocr to text file (the log)
-        logging.info('extraction successful, {filenameWoExt}, ')
-        # remove temporary tiff files
-        remove(os.path.dirname(tiffFiles[0]))
-        # save to pdf
-        saveDir = f'.\\exported\\{filePath[6:]}\\{filenameWoExt}.pdf'
-        ocrTxt.writerow([filenameWoExt, extracted_text])
-        csvFile.close()
+        tiffFiles = shredPdf(src_path, tmpDir) # shreds pdf into TIFF files, for OCRing
+
+        if len(tiffFiles) > 0: # confirms there were no errors shredding
+            # pre-process image
+            tmpPdfFiles = []
+            
+            for img_path in tiffFiles: # OCR's each image, saves temporary pdf
+                processedImg = preProcess(img_path)
+
+                # OCR it! psm = 1 will recognize 2-column layouts.
+                pdf = pytesseract.image_to_pdf_or_hocr(processedImg, extension='pdf', config='--psm 1')
+                text = pytesseract.image_to_string(processedImg,lang='eng', config='--psm 1')
+                # save extracted text to variable
+                extracted_text = extracted_text + text
+                # save pdf to temp path
+                tmpSaveDir = f'{img_path[:-5]}.pdf'
+                with open(tmpSaveDir, 'w+b') as f:
+                    f.write(pdf) # pdf type is bytes by default
+                    print(f'saved temp pdf: {tmpSaveDir}')
+                    tmpPdfFiles.append(tmpSaveDir)
+            
+            # merges temporary pdfs
+            mergePdfs(tmpPdfFiles)
+            
+            # remove temporary tiff files
+            remove(os.path.dirname(tiffFiles[0]))
+
+            # save to pdf
+            #saveDir = f'.\\exported\\{filePath[6:]}\\{filenameWoExt}.pdf'
+            
+            # save to text
+            q.put([filenameWoExt, extracted_text, ''])
+
+        return 1
+
+    except Exception: # if aaany error pops up, print it and save to file the ocr which failed
+        q.put([filenameWoExt, extracted_text, 'OCR_FAILED'])
+        traceback.print_exc()
+        return 0
 
 def shredPdf(src_path, destDir):
     # shreds pdfs into single-paged images
@@ -169,6 +211,18 @@ def mergePdfs(pdfFiles):
     print(f'saved ' + f'\\exported\\{filename[:-6]}.pdf')
 
 if __name__ == "__main__":
+    # initializes multiprocessing
+    cpuCount = multiprocessing.cpu_count() + 1 # adds one for the file-writing listener
+    print(f'there are {cpuCount-1} cores on this computer. Using all of them.')
+    pool = multiprocessing.Pool(processes=cpuCount)
+
+    # initializes multiprocessing logger
+    manager = multiprocessing.Manager()
+    q = manager.Queue()
+    watcher = pool.apply_async(ocrTxtListener, (q,))
+    
+    x = 3
+
     patterns = ["*.pdf", "*.PDF", "*.tiff"]
     ignore_patterns = ""
     ignore_directories = True
@@ -192,4 +246,4 @@ if __name__ == "__main__":
             time.sleep(2)
     except KeyboardInterrupt:
         my_observer.stop()
-        my_observer.join()
+        my_observer.join()    
